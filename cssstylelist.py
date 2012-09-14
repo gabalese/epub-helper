f64f8c244f971ff07208cb779bd93dbcfee57422
gabalese <gabriele@alese.it>
2012-09-14 23:13:13 +0200

    modified cssstylelist.py

--- cssstylelist.py
+++ cssstylelist.py
@@ -1,42 +1,83 @@
 #! /usr/bin/env python3
 # file: cssstylelist.py
-# Make a list of every style used in html and returns that
-# From the CLI, type `./cssstylelist.py > file_with_a_list.txt
-# Feel free to address any complain to @gabalese
+# Make a list of every style used in html and print to stdout
 
 import os, glob, sys
+import zipfile as ZIP
+from lxml import etree as ET
+
 try:
 	from lxml import etree as ET
 except ImportError:
-	import xml.etree.ElementTree as ET
-	print("lxml not installed. Running with xml.etree instead")
+	print("lxml not installed.")
+	sys.exit()
 
-path = "OEBPS/Text" # your mileage may vary
 list = []
 new_list = []
+filelist = []
+root_folder = ""
+namespaces = namespaces = {"opf":"http://www.idpf.org/2007/opf","dc":"http://purl.org/dc/elements/1.1/"}
 
-def cssList():
+def cssList(infile):
 	global list
 	global new_list 
-	for infile in glob.glob(os.path.join(path, '*html')):
-		try:
-			html = ET.parse(infile).getroot()
-		except:
-			print("ERROR: Unable to parse " + infile)
-			print("This is likely to happen with ill-formed xhtml files.")
-			sys.exit(1)
-		for i in html.iter():
-			list.append(i.get("class"))
+	# init a tolerant parser
+	parser = ET.HTMLParser()
+	html = ET.fromstring(infile,parser)
+	
+	for i in html.iter():
+		list.append(i.get("class"))
+
+
+def parseInfo(file):
+	info = {}
+	global root_folder
+	try:
+		f = ZIP.ZipFile(file).read("META-INF/container.xml")
+	except KeyError:
+		print( "The %s file is not a valid OCF." % str(file) )
+	try:
+		f = ET.fromstring(f)
+		info["path_to_opf"] = f[0][0].get("full-path")
+		root_folder = os.path.dirname(info["path_to_opf"])
+	except:
+		pass
+	opf = ET.fromstring(ZIP.ZipFile(file).read(info["path_to_opf"]))
+	
+	id = opf.xpath("//opf:spine",namespaces=namespaces)[0].get("toc")
+	expr = "//*[@id='%s']" % id
+	info["ncx_name"] = opf.xpath(expr)[0].get("href")
+	info["path_to_ncx"] = root_folder + "/" + info["ncx_name"]
+	info.pop("ncx_name")
+	return info
+	
+def parseOPF(file):
+	meta = {}
+	opf = ET.fromstring(ZIP.ZipFile(file).read(parseInfo(file)["path_to_opf"]))
+	
+	return opf
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("USAGE: \
+		./cssstylelist.py <epubfile>")
+		sys.exit()
+		
+	i = sys.argv[1]
+	opf = parseOPF(i)
+	for item in opf.xpath("//opf:item",namespaces=namespaces):
+		if item.get("media-type") == "application/xhtml+xml":
+			filelist.append(item.get("href"))
+	
+	for item in filelist:
+		cssList(ZIP.ZipFile(sys.argv[1]).read(root_folder+"/"+item))
 	
 	for i in list:
 		if i not in new_list:
 			if i is not None:
 				new_list.append(i)
-			
-	return new_list	
-
-
 	
-if __name__ == "__main__":
-	for item in (cssList()):
-		print(item)
+	for i in new_list:
+		print(i)
